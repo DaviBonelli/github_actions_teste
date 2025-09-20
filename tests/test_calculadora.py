329222ec571dc20aa24035efea8fd1901dfce8e3
Davi Bonelli Rodrigues <davi33013@gmail.com>
2025-09-20 12:39:41 -0300

    teste 1
    teste 1

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,6 +1,6 @@
 # tests/test_calculadora.py
 import pytest
-from src.calculadora import somar, dividir
+from src.calculadora import somar, dividir, multiplicar, subtrair
 
 def test_somar():
     assert somar(2, 3) == 5
@@ -17,5 +17,5 @@ def test_multiplicacao():
         multiplicar(0, 5) == 0
 
 def test_subtracao():
-    assert dividir(10, 2) == 8
-    assert somar(4, 1) == 3
+    assert subtrair(10, 2) == 8
+    assert subtrair(4, 1) == 3
