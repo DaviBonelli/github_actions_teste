9378774a3d9a86026f86ccf5dcfd579851487317
Davi Bonelli Rodrigues <davi33013@gmail.com>
2025-09-20 12:37:32 -0300

    teste
    teste

---
+++ src/calculadora.py
@@ -0,0 +1,16 @@
+def somar(a, b):
+    return a + b
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("Divisão por zero!")
+    return a / b
+
+def multiplicacao(a, b):
+      if b == 0:
+        raise ValueError("O valor é zero!")
+    return a * b
+
+def subtracao(a, b):
+    return a - b
+#
---
+++ tests/test_calculadora.py
@@ -0,0 +1,21 @@
+# tests/test_calculadora.py
+import pytest
+from src.calculadora import somar, dividir
+
+def test_somar():
+    assert somar(2, 3) == 5
+    assert somar(-1, 1) == 0
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+    with pytest.raises(ValueError):
+        dividir(10, 0)
+
+def test_multiplicacao():
+    assert multiplicar(2, 3) == 6
+    with pytest.raises(ValueError):
+        multiplicar(0, 5) == 0
+
+def test_subtracao():
+    assert dividir(10, 2) == 8
+    assert somar(4, 1) == 3
